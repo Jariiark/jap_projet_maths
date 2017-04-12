38737256adb101aa92dc3ff0e2830ed336ebc04b
Unknown <derriick.ensiie@yahoo.com>
2017-04-12 14:56:35 +0200

    InitData fonctionnel (normalement)

--- src/jerrikk0-mhd.cpp
+++ src/jerrikk0-mhd.cpp
@@ -13,32 +13,32 @@ void Ref2PhysMap(real *xx, real *yy, real *x, real *y)
 // primitives {{{
 void primitives(real * Y, real * W)
 {
-	Y[1] = W[1];
-	Y[2] = W[2]/ W[1];
-	Y[3] = W[3]/ W[1];
-	Y[4] = W[4]/ W[1];
+	Y[0] = W[0];
+	Y[1] = W[1]/ W[0];
+	Y[2] = (_GAM - 1) * (W[2] - W[0]/2 * ((W[1]/W[0] * W[1]/W[0]) + (W[3]/W[0] * W[3]/W[0]) + (W[4]/W[0] * W[4]/W[0]) - 1./2 * (W[5]*W[5] + W[6]*W[6] + W[7]*W[7])));
+	Y[3] = W[3]/ W[0];
+	Y[4] = W[4]/ W[0];
+	Y[5] = W[5];
 	Y[6] = W[6];
 	Y[7] = W[7];
 	Y[8] = W[8];
-	Y[5] = (_GAM - 1) * (W[5] - W[1]/2 * ((W[2]/W[1] * W[2]/W[1]) + (W[3]/W[1] * W[3]/W[1]) + (W[4]/W[1] * W[4]/W[1]) - 1/2 * (W[6]*W[6] + W[7]*W[7] + W[8]*W[8])));
 }
 // }}}
 
 // conservatives {{{
 void conservatives(real * Y, real * W)
 {
-	W[1] = Y[1];
-	W[2] = Y[2] * Y[1];
-	W[3] = Y[3] * Y[1];
-	W[4] = Y[4] * Y[1];
+	W[0] = Y[0];
+	W[1] = Y[1] * Y[0];
+	W[2] = Y[2]/(_GAM - 1) + Y[0]/2 * (Y[1]*Y[1] + Y[3]*Y[3] + Y[4]*Y[4]) + 1./2 * (Y[5]*Y[5] + Y[6]*Y[6] + Y[7]*Y[7]);
+	W[3] = Y[3] * Y[0];
+	W[4] = Y[4] * Y[0];
+	W[5] = Y[5];
 	W[6] = Y[6];
 	W[7] = Y[7];
 	W[8] = Y[8];
-	//W[5] = (Y[5]+1/2*(Y[6]*Y[6]+Y[7]*Y[7]+Y[8]*Y[8]))/(_GAM-1)+Y[1]/2*(Y[2]*Y[2]+Y[3]*Y[3]+Y[4]*Y[4]);
-	W[5] = Y[5]/(_GAM - 1) + Y[1]/2 * (Y[2]*Y[2] + Y[3]*Y[3] + Y[4]*Y[4]) + 1./2 * (Y[6]*Y[6] + Y[7]*Y[7] + Y[8]*Y[8]);
 }
 // }}}
-
 
 // InitData {{{
 /*
@@ -49,33 +49,19 @@ void conservatives(real * Y, real * W)
  */
 void InitData(real *Wn1)
 {
-	real x, y, xx, yy;
-	real W[_M], Y[_M], consGam[_M];
-	real gam[_M] = {_GAM, _GAM, _GAM, _GAM, _GAM, _GAM, _GAM, _GAM, _GAM};
-	
-// 	for (int i=0; i<_LONGUEURX; i++) {
-// 		for(int j=0; j<_LONGUEURY; j++){
-// 			xx = i*1.0/_NXTRANSBLOCK;
-// 			yy = j*1.0/_NYTRANSBLOCK;
-// 			Ref2PhysMap(&xx, &yy, &x, &y);
-// 			Wexact(&x, &y, W);
-// 			for(int iv=0;iv<_M;iv++){
-// 				Wn1[iv*(int)_LONGUEURX*(int)_LONGUEURY+i+j]=W[iv];
-// 			}
-// 		}
-// 	}
+	real W[_M];
 	
 	for(int j = 0; j < _NY; j++) {
 		for(int i = 0; i < _NX; i++) {
 			real xx = 1. / _NX * (i + 0.5);
-			real yy = 0;
+			real yy = 1. / _NY * (j + 0.5);
 			real x, y;
 			
 			Ref2PhysMap(&xx, &yy, &x, &y);
 			Wexact(&x, &y, W);
 			
 			for (int k = 0; k < _M; ++k) {
-				Wn1[i + 0 * _NXTRANSBLOCK * _NYTRANSBLOCK + j * _NXTRANSBLOCK] = W[i];
+				Wn1[i + k * _NXTRANSBLOCK * _NYTRANSBLOCK + j * _NXTRANSBLOCK] = W[k];
 			}
 		}
 	}
@@ -86,20 +72,34 @@ void InitData(real *Wn1)
 void TimeStepCPU_1D(real *Wn1, real *dtt)
 {
 	real W[_M], f[_M];
+	real vnX[3] = {1, 0, 0};
 	
 	for(int j = 0; j < _NY; j++) {
 		for(int i = 0; i < _NX; i++) {
 			for (int k = 0; k < _M; ++k) {
 				W[k] = Wn1[i + k * _NXTRANSBLOCK * _NYTRANSBLOCK + j * _NXTRANSBLOCK];
-				flux(W, dtt, f);
+				flux(W, vnX, f);
 				Wn1[i + k * _NXTRANSBLOCK * _NYTRANSBLOCK + j * _NXTRANSBLOCK] = f[k];
 			}
 		}
 	}
 }
 
-void TimeStepCPU_2D(real *, real *)
+void TimeStepCPU_2D(real *Wn1, real *dtt)
 {
+	real W[_M], fX[_M], fY[_M];
+	real vnX[3] = {1, 0, 0};
+	real vnY[3] = {0, 1, 0};
 	
+	for(int j = 0; j < _NY; j++) {
+		for(int i = 0; i < _NX; i++) {
+			for (int k = 0; k < _M; ++k) {
+				W[k] = Wn1[i + k * _NXTRANSBLOCK * _NYTRANSBLOCK + j * _NXTRANSBLOCK];
+				flux(W, vnX, fX);
+				flux(fX, vnY, fY);
+				Wn1[i + k * _NXTRANSBLOCK * _NYTRANSBLOCK + j * _NXTRANSBLOCK] = fY[k];
+			}
+		}
+	}
 }
 // }}}
