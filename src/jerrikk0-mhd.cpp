f307838729f51a34bddc07bcafd73ae5882e47c9
Unknown <derriick.ensiie@yahoo.com>
2017-04-13 11:34:30 +0200

    TimeStep1D mdr cé joli

--- src/jerrikk0-mhd.cpp
+++ src/jerrikk0-mhd.cpp
@@ -17,7 +17,7 @@ void primitives(real * Y, real * W)
 	Y[1] = W[1]/ W[0];
 	Y[3] = W[3]/ W[0];
 	Y[4] = W[4]/ W[0];
-	Y[2] = (_GAM - 1) * (W[2] - W[0]/2 * ((Y[1]*Y[1]) + (Y[3]*Y[3] + Y[4]*Y[4]) - 1./2 * (W[5]*W[5] + W[6]*W[6] + W[7]*W[7])));
+	Y[2] = (_GAM - 1) * (W[2] - W[0]/2 * (Y[1]*Y[1] + Y[3]*Y[3] + Y[4]*Y[4])) - 0.5 * (W[5]*W[5] + W[6]*W[6] + W[7]*W[7]);
 	Y[5] = W[5];
 	Y[6] = W[6];
 	Y[7] = W[7];
@@ -30,7 +30,7 @@ void conservatives(real * Y, real * W)
 {
 	W[0] = Y[0];
 	W[1] = Y[1] * Y[0];
-	W[2] = Y[2]/(_GAM - 1) + Y[0]/2 * (Y[1]*Y[1] + Y[3]*Y[3] + Y[4]*Y[4]) + 1./2 * (Y[5]*Y[5] + Y[6]*Y[6] + Y[7]*Y[7]);
+	W[2] = Y[2]/(_GAM - 1) + Y[0]/2 * (Y[1]*Y[1] + Y[3]*Y[3] + Y[4]*Y[4]) + 0.5 * (Y[5]*Y[5] + Y[6]*Y[6] + Y[7]*Y[7]);
 	W[3] = Y[3] * Y[0];
 	W[4] = Y[4] * Y[0];
 	W[5] = Y[5];
@@ -81,64 +81,164 @@ void InitData(real *Wn1)
 }
 // }}}
 
+void flux(real *WL, real *WR, real *vn, real *fluxLR)
+{
+	real fluxL[_M];
+	real fluxR[_M];
+	
+	flux(WL, vn, fluxL);
+	flux(WR, vn, fluxR);
+	
+	for (int i = 0; i < _M; ++i) {
+		fluxLR[i] = 0.5 * (fluxL[i] + fluxR[i]) - _CH * 0.5 * (WR[i] - WL[i]);
+	}
+}
+
 // TimeStepCPU {{{
 void TimeStepCPU_1D(real *Wn1, real *dtt)
 {
 	real WL[_M];
-	real WR[_M];
 	real WM[_M];
-	real fluxWL[_M];
-	real fluxWR[_M];
+	real WR[_M];
+	real fluxLM[_M];
+	real fluxMR[_M];
 	real vnX[3] = {1, 0, 0};
-	real dx = (_XMAX - _XMIN) / _NX;
-	real xMin = _XMIN;
-	real xMax = _XMAX;
-	real yZero = 0;
+	real dx = (real)_LONGUEURX / _NBWORKSX;
+	real Wn2[_NX * _NY * _M];
 	
-	for (int j = 0; j < _NYTRANSBLOCK; ++j) {
-		for (int i = 0; i < _NXTRANSBLOCK; ++i) {
+	*dtt = _CFL * dx / _CH;
+	//*dtt = .000001;
+	//cout << "dx= " << dx << endl << "dt= " << *dtt << endl;
+	
+	for (int j = 0; j < _NY; ++j) {
+		for (int i = 0; i < _NX; ++i) {
 			for (int k = 0; k < _M; ++k) {
+				WM[k] = Wn1[i + k * _NX * _NY + j * _NX];
 				
-				if (i <= 0) {
-					Wexact(&xMin, &yZero, WL);
-					WR[k] = Wn1[i + 1 + k * _NXTRANSBLOCK * _NYTRANSBLOCK + j * _NXTRANSBLOCK];
+				if (i == 0) {
+					real xx = 1. / _NX * (i + 0.5);
+					real yy = 1. / _NY * (j + 0.5);
+					real x, y;
+					Ref2PhysMap(&xx, &yy, &x, &y);
+					
+					Wexact(&x, &y, WL);
+					//WL[k] = 0;
+					WR[k] = Wn1[i + 1 + k * _NX * _NY + j * _NX];
 				}
-				else if (i >= _NXTRANSBLOCK) {
-					WL[k] = Wn1[i - 1 + k * _NXTRANSBLOCK * _NYTRANSBLOCK + j * _NXTRANSBLOCK];
-					Wexact(&xMax, &yZero, WR);
+				else if (i == _NX - 1) {
+					real xx = 1. / _NX * (i + 0.5);
+					real yy = 1. / _NY * (j + 0.5);
+					real x, y;
+					Ref2PhysMap(&xx, &yy, &x, &y);
+					
+					WL[k] = Wn1[i - 1 + k * _NX * _NY + j * _NX];
+					Wexact(&x, &y, WR);
+					//WR[k] = 0;
 				}
 				else {
-					WL[k] = Wn1[i - 1 + k * _NXTRANSBLOCK * _NYTRANSBLOCK + j * _NXTRANSBLOCK];
-					WL[k] = Wn1[i + 1 + k * _NXTRANSBLOCK * _NYTRANSBLOCK + j * _NXTRANSBLOCK];
-				}
-			
-				flux(WL, vnX, fluxWL);
-				flux(WR, vnX, fluxWR);
+					WL[k] = Wn1[i - 1 + k * _NX * _NY + j * _NX];
+					WR[k] = Wn1[i + 1 + k * _NX * _NY + j * _NX];
+				}				
 				
-				for (int k = 0; k < _M; ++k) {
-					int n = i + k * _NXTRANSBLOCK * _NYTRANSBLOCK + j * _NXTRANSBLOCK;
-					Wn1[n] = Wn1[n] - (1./2 * (real)(fluxWR[k] + fluxWL[k]) - _CH/2 * (WR[k] - WL[k]));
-				}
+				flux(WL, WM, vnX, fluxLM);
+				flux(WM, WR, vnX, fluxMR);
+					
+				int n = i + k * _NX * _NY + j * _NX;
+				Wn2[n] = Wn1[n] - *dtt/dx * (fluxMR[n] - fluxLM[n]);
 			}
 		}
 	}
+	
+	for (int i = 0; i < _NX * _NY * _M; ++i)
+		Wn1[i] = Wn2[i];
 }
 
 void TimeStepCPU_2D(real *Wn1, real *dtt)
 {
-	real W[_M], fX[_M], fY[_M];
+	real WL[_M];
+	real WM[_M];
+	real WU[_M];
+	real WD[_M];
+	real WR[_M];
+	real fluxLM[_M];
+	real fluxMR[_M];
+	real fluxUM[_M];
+	real fluxMD[_M];
 	real vnX[3] = {1, 0, 0};
 	real vnY[3] = {0, 1, 0};
+	real dx = (real)_LONGUEURX / _NBWORKSX;
+	real dy = (real)_LONGUEURY / _NBWORKSY;
+	real Wn2[_NX * _NY * _M];
 	
-	for(int j = 0; j < _NY; j++) {
-		for(int i = 0; i < _NX; i++) {
+	*dtt = _CFL * dx / _CH;
+	//*dtt = .000001;
+	//cout << "dx= " << dx << endl << "dt= " << *dtt << endl;
+	
+	for (int j = 0; j < _NY; ++j) {
+		for (int i = 0; i < _NX; ++i) {
 			for (int k = 0; k < _M; ++k) {
-				W[k] = Wn1[i + k * _NXTRANSBLOCK * _NYTRANSBLOCK + j * _NXTRANSBLOCK];
-				flux(W, vnX, fX);
-				flux(fX, vnY, fY);
-				Wn1[i + k * _NXTRANSBLOCK * _NYTRANSBLOCK + j * _NXTRANSBLOCK] = fY[k];
+				WM[k] = Wn1[i + k * _NX * _NY + j * _NX];
+				
+				if (i == 0) {
+					real xx = 1. / _NX * (i + 0.5);
+					real yy = 1. / _NY * (j + 0.5);
+					real x, y;
+					Ref2PhysMap(&xx, &yy, &x, &y);
+					
+					Wexact(&x, &y, WL);
+					//WL[k] = 0;
+					WR[k] = Wn1[i + 1 + k * _NX * _NY + j * _NX];
+				}
+				else if (i == _NX - 1) {
+					real xx = 1. / _NX * (i + 0.5);
+					real yy = 1. / _NY * (j + 0.5);
+					real x, y;
+					Ref2PhysMap(&xx, &yy, &x, &y);
+					
+					WL[k] = Wn1[i - 1 + k * _NX * _NY + j * _NX];
+					Wexact(&x, &y, WR);
+					//WR[k] = 0;
+				}
+				else {
+					WL[k] = Wn1[i - 1 + k * _NX * _NY + j * _NX];
+					WR[k] = Wn1[i + 1 + k * _NX * _NY + j * _NX];
+				}
+				
+				if (j == 0) {
+					real xx = 1. / _NX * (i + 0.5);
+					real yy = 1. / _NY * (j + 0.5);
+					real x, y;
+					Ref2PhysMap(&xx, &yy, &x, &y);
+					
+					Wexact(&x, &y, WU);
+					WD[k] = Wn1[i + k * _NX * _NY + (j + 1) * _NX];
+				}
+				else if (j == _NY - 1) {
+					real xx = 1. / _NX * (i + 0.5);
+					real yy = 1. / _NY * (j + 0.5);
+					real x, y;
+					Ref2PhysMap(&xx, &yy, &x, &y);
+					
+					WU[k] = Wn1[i + k * _NX * _NY + (j - 1) * _NX];
+					Wexact(&x, &y, WD);
+				}
+				else {
+					WU[k] = Wn1[i + k * _NX * _NY + (j - 1) * _NX];
+					WD[k] = Wn1[i + k * _NX * _NY + (j + 1) * _NX];
+				}				
+				
+				flux(WL, WM, vnX, fluxLM);
+				flux(WM, WR, vnX, fluxMR);
+				flux(WU, WM, vnX, fluxUM);
+				flux(WM, WD, vnX, fluxMD);
+				
+				int n = i + k * _NX * _NY + j * _NX;
+				Wn2[n] = Wn1[n] - *dtt/dx * (fluxMR[n] - fluxLM[n]) - *dtt/dy * (fluxUM[n] - fluxMD[n]);
 			}
 		}
 	}
+	
+	for (int i = 0; i < _NX * _NY * _M; ++i)
+		Wn1[i] = Wn2[i];
 }
 // }}}
